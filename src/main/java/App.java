112f11e4cd204db1d0c1e0849825b12e0abf77ab
Bill Tucker <bill.h.tucker@gmail.com>
2021-03-27 23:20:28 -0600

    add examples from json-schema.org

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,4 +1,6 @@
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.concurrent.Callable;
 import org.everit.json.schema.Schema;
@@ -8,7 +10,7 @@ import org.json.JSONTokener;
 import picocli.CommandLine;
 
 @CommandLine.Command(name="JsonSchemaTester", mixinStandardHelpOptions=true, version="JsonSchemaTester 1.0")
-public class App implements Callable {
+public class App implements Runnable {
 
     @CommandLine.Parameters(index = "0", description = "Schema file to compare json files against.")
     private static File jsonSchema;
@@ -16,23 +18,32 @@ public class App implements Callable {
     @CommandLine.Parameters(index = "1", description = "The json file to test against schema.")
     private static File jsonFile;
 
-    public Integer call() throws Exception {
-        return 0;
+    public void run() {
+//        if (jsonSchema.exists()){
+//            System.out.println(String.format("found jssonSChema file: %s", jsonSchema.getName()));
+//        }
+//        else {
+//
+//            System.out.println(String.format("did not find jssonSChema file: %s", jsonSchema.getName()));
+//        }
+        try {
+
+            try (FileInputStream fisSchema = new FileInputStream(jsonSchema); FileInputStream fisJson = new FileInputStream(jsonFile)) {
+                JSONObject rawSchema = new JSONObject(new JSONTokener(fisSchema));
+                Schema schema = SchemaLoader.load(rawSchema);
+                JSONObject rawJson = new JSONObject(new JSONTokener(fisJson));
+//                schema.validate(new JSONObject("{\"hello\" : \"world\"}")); // throws a ValidationException if this object is invalid
+                schema.validate(rawJson); // throws a ValidationException if this object is invalid
+            }
+
+        }
+        catch (IOException err) {
+            System.out.println(String.format("did not find jssonSChema file: %s", jsonSchema.getName()));
+        }
 
     }
     public static void main(String... args) {
-//        int exitCode = new CommandLine(new CheckSum()).execute(args);
-//        try (InputStream inputStream = getclass().getClassLoader().getResourceAsStream(jsonSchema)) {
-//            JSONObject rawSchema = new JSONObject(new JSONTokener(inputStream));
-//            Schema schema = SchemaLoader.load(rawSchema);
-//            schema.validate(new JSONObject("{\"hello\" : \"world\"}")); // throws a ValidationException if this object is invalid
-//        }
-        if (jsonSchema.exists()){
-            System.out.println(String.format("found jssonSChema file: %s", jsonSchema.getName()));
-        }
-
         System.out.println("helloworld");
-        int exitCode = 0;
-        System.exit(exitCode);
+        System.exit(new CommandLine(new App()).execute(args));
     }
 }
